b052af6f0c2c98e2c008f0bcfc32caea9b295d75
k1729326490 <118305262+k1729326490@users.noreply.github.com>
2024-04-09 10:51:46 +0800

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -2,4 +2,14 @@ function hello(name){
     return(`hello ${name}`);
 }
 
-export {hello}
+function celsius2fahrenheit(celsius){
+    const fahrenheit = celsius *9 /5 + 32;
+    return(fahrenheit);
+}
+
+function sf2acres(sf){
+    const acres = sf/43560
+    return(acres);
+}
+
+export {hello, celsius2fahrenheit, sf2acres}
