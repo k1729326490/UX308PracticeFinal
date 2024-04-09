f3abd5e304cc6f2c60c08a643baff02efb9c417f
k1729326490 <118305262+k1729326490@users.noreply.github.com>
2024-04-09 10:52:16 +0800

    Update components.jsx

--- src/components.jsx
+++ src/components.jsx
@@ -1,13 +1,31 @@
-import {hello} from './functions.js';
+import {hello,celsius2fahrenheit, sf2acres} from './functions.js';
 
 function Question1(){
     return <section>
 1. Write a function that takes a _name_ as an argument and _returns a string_ that _contains "hello"_ and the name paramerer. Test at least 3 names. <br />*Note ... I solved this one already
       <h2>results</h2>
-      <p>hello("Rich") == "{hello('Rich')}"</p>
-      <p>hello("Bill") == "{hello('Bill')}"</p>
-      <p>hello("Chris") == "{hello('Chris')}"</p>
+      <p>hello("You") == "{hello('You')}"</p>
+      <p>hello("Me") == "{hello('Me')}"</p>
+      <p>hello("Everyone") == "{hello('Everyone')}"</p>
     </section>;
 }
 
-export {Question1}
+function Question2(){
+    return <section>
+2.Write a function that converts celsius to fahrenheit
+<h2>reuslt</h2>
+<p>celsius2fahrenheit(0) == "{celsius2fahrenheit(0)}"</p>
+<p>celsius2fahrenheit(100) == "{celsius2fahrenheit(100)}"</p>
+<p>celsius2fahrenheit(36) == "{celsius2fahrenheit(36)}"</p>
+    </section>
+}
+
+function Question3(){
+    return <section>
+3. There are 43,560 square feet per acre. Write a program that converts square feet to acres. Test at least 3 conversions.
+    <h2>Results</h2>
+    <p>sf2acres(10000) == {sf2acres(10000)}</p>
+    </section>
+}
+
+export {Question1, Question2, Question3}
