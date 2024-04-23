8f36e0ed8e79c70dafa74b65b87967685dc12ced
k1729326490 <118305262+k1729326490@users.noreply.github.com>
2024-04-24 04:15:43 +0800

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -2,14 +2,40 @@ function hello(name){
     return(`hello ${name}`);
 }
 
-function celsius2fahrenheit(celsius){
-    const fahrenheit = celsius *9 /5 + 32;
-    return(fahrenheit);
-}
+function computeAirQuality(number){
+    let air;
+    if(number < 50){
+      air = 'Good';
+    } else if(number < 100){
+      air = 'Moderate';
+    } else if(number < 150){
+      air = 'Unhealthy for Sensitive Groups';
+    } else if(number < 200){
+      air = 'Unhealthy';
+    } else if(number < 300){
+      air = 'Very Unhealthy';
+    } else {
+      air = 'Hazardous';
+    }
+  
+    return `If the air quality index is ${number} AQI, the air quality is ${air}`;
+  }
 
 function sf2acres(sf){
     const acres = sf/43560
     return(acres);
 }
 
-export {hello, celsius2fahrenheit, sf2acres}
+function calculateSlope(x1, y1, x2, y2) {
+    const rise = y2 - y1;
+    const run = x2 - x1;
+  
+    if (run === 0) {
+      return "Infinity";
+    }
+  
+    const slope = rise / run;
+    return slope;
+  }
+
+export {hello, computeAirQuality, sf2acres, calculateSlope}
