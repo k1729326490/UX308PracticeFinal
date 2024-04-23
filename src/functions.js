680a328909aaf82681ce0093fbb9405e74e1cca6
k1729326490 <118305262+k1729326490@users.noreply.github.com>
2024-04-24 05:06:15 +0800

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,41 +1,96 @@
-function hello(name){
-    return(`hello ${name}`);
+
+
+function colour_mix(rgb_colour1, rgb_colour2) {
+
+  const mixMap = {
+    "redblue": "fuchsia",
+    "redgreen": "yellow",
+    "greenblue": "aqua",
+    "redred": "red",
+    "blueblue": "blue",
+    "greengreen": "green"
+  };
+
+  const colours = [rgb_colour1.toLowerCase(), rgb_colour2.toLowerCase()].sort().join("");
+
+  if (!(colours in mixMap)) {
+    return "Error";
+  }
+
+
+  return mixMap[colours];
 }
 
-function computeAirQuality(number){
-    let air;
-    if(number < 50){
-      air = 'Good';
-    } else if(number < 100){
-      air = 'Moderate';
-    } else if(number < 150){
-      air = 'Unhealthy for Sensitive Groups';
-    } else if(number < 200){
-      air = 'Unhealthy';
-    } else if(number < 300){
-      air = 'Very Unhealthy';
+
+
+
+function largest_product(val1, val2, val3) {
+ 
+  const minVal = Math.min(val1, val2, val3);
+  const minValIndex = [val1, val2, val3].indexOf(minVal);
+  const vals = [val1, val2, val3];
+  vals.splice(minValIndex, 1);
+  const secondMinVal = Math.min(...vals);
+
+  return Math.max(val1, val2, val3) * secondMinVal;
+}
+
+
+function day_of_the_week(day_num) {
+
+  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+  if (day_num < 1 || day_num > 7) {
+    return "Error";
+  }
+
+
+  return daysOfWeek[day_num - 1];
+}
+
+function pay_raise(status, years, salary) {
+  let raisePercentage;
+
+  if (status === 'F') {
+    if (years >= 10) {
+      raisePercentage = 0.05;
+    } else if (years < 4) {
+      raisePercentage = 0.015;
     } else {
-      air = 'Hazardous';
+      raisePercentage = 0.02;
     }
-  
-    return `If the air quality index is ${number} AQI, the air quality is ${air}`;
+  } else if (status === 'P') {
+    if (years >= 10) {
+      raisePercentage = 0.03;
+    } else if (years < 4) {
+      raisePercentage = 0.01;
+    } else {
+      raisePercentage = 0.02;
+    }
+  } else {
+    return "Error: Invalid employment type";
   }
 
-function sf2acres(sf){
-    const acres = sf/43560
-    return(acres);
+
+  const raiseAmount = salary * raisePercentage;
+  const newSalary = salary + raiseAmount;
+
+  return newSalary.toFixed(2);
 }
 
-function calculateSlope(x1, y1, x2, y2) {
-    const rise = y2 - y1;
-    const run = x2 - x1;
-  
-    if (run === 0) {
-      return "Infinity";
-    }
-  
-    const slope = rise / run;
-    return slope;
-  }
 
-export {hello, computeAirQuality, sf2acres, calculateSlope}
+function is_leap(year) {
+
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+
+export default {colour_mix, largest_product, day_of_the_week, pay_raise, is_leap};
+
+
